97ca61c5c40b452fae31ffb6744139ec2bc0e11f
yangtao <1157661964@qq.com>
2024-04-25 00:07:33 +0800

    hotfix fix highlights with ibl.lua

--- lua/plugins/ibl.lua
+++ lua/plugins/ibl.lua
@@ -1,3 +1,13 @@
+vim.cmd [[
+  highlight IndentBlanklineIndent1 guifg=#E78284
+  highlight IndentBlanklineIndent2 guifg=#dbb75a
+  highlight IndentBlanklineIndent3 guifg=#E5C890
+  highlight IndentBlanklineIndent4 guifg=#A6D189
+  highlight IndentBlanklineIndent5 guifg=#81C8BE
+  highlight IndentBlanklineIndent6 guifg=#8CAAEE
+  highlight IndentBlanklineIndent7 guifg=#764cd9
+]]
+
 return {
   {
     'lukas-reineke/indent-blankline.nvim',
