656bee0656a679fa21a838f8c81b2eef335a48ae
yangtao <1157661964@qq.com>
2024-04-25 23:20:56 +0800

    fix fold highlight and add bl for end a dap

--- lua/core/keymaps/dap.lua
+++ lua/core/keymaps/dap.lua
@@ -1,7 +1,6 @@
 local keymap = vim.keymap
 
 keymap.set('n', 'b', '<nop>')
-keymap.set('n', 'bb', '<cmd>lua require"dap".toggle_breakpoint()<CR>')
 -- 相当于toggel方法
 keymap.set({"n", "v"}, "br", ":DapRestartFrame<CR>",
   {silent = true, noremap = true, buffer = bufnr})
@@ -14,4 +13,6 @@ keymap.set({"n", "v"}, "bo", ":DapStepOut<CR>",
 keymap.set({"n", "v"}, "bb", "<cmd>lua require'dap'.toggle_breakpoint()<CR>",
   {silent = true, noremap = true, buffer = bufnr})
 keymap.set("n", "bc", "<cmd> lua require'dapui'.toggle()<CR>",
+  {silent = true, noremap = true, buffer = bufnr})
+keymap.set({"n", "v"}, "bl", "<cmd>lua require'dap'.run_last()<CR>",
   {silent = true, noremap = true, buffer = bufnr})
