b683ce7c64cf4f2b70cf5464d55f45d438fc5637
yangtao <1157661964@qq.com>
2024-04-26 15:56:41 +0800

    finetune dap-ui ui

--- lua/dap/dapui.lua
+++ lua/dap/dapui.lua
@@ -1,53 +1,69 @@
+---@diagnostic disable: undefined-global
 local dap_breakpoint_color = {
-    breakpoint = {
-        fg='#993939',
-        bg='#31353f',
-    },
-    logpoing = {
-        fg='#61afef',
-        bg='#31353f',
-    },
-    stopped = {
-        fg='#98c379',
-        bg='#31353f'
-    },
+  breakpoint = {
+    fg='#993939',
+    bg='#31353f',
+  },
+  logpoing = {
+    fg='#61afef',
+    bg='#31353f',
+  },
+  stopped = {
+    fg='#98c379',
+    bg='#31353f'
+  },
+  breakpoint_icon = {
+    fg='#993939',
+    bg=none,
+  },
+  logpoing_icon = {
+    fg='#61afef',
+    bg=none,
+  },
+  stopped_icon = {
+    fg='#98c379',
+    bg=none,
+  },
 }
 
 vim.api.nvim_set_hl(0, 'DapBreakpoint', dap_breakpoint_color.breakpoint)
 vim.api.nvim_set_hl(0, 'DapLogPoint', dap_breakpoint_color.logpoing)
 vim.api.nvim_set_hl(0, 'DapStopped', dap_breakpoint_color.stopped)
+vim.api.nvim_set_hl(0, 'DapBreakpoint_icon', dap_breakpoint_color.breakpoint_icon)
+vim.api.nvim_set_hl(0, 'DapLogPoint_icon', dap_breakpoint_color.logpoing_icon)
+vim.api.nvim_set_hl(0, 'DapStopped_icon', dap_breakpoint_color.stopped_icon)
 
 local dap_breakpoint = {
-    error = {
-        text = "",
-        texthl = "DapBreakpoint",
-        linehl = "DapBreakpoint",
-        numhl = "DapBreakpoint",
-    },
-    condition = {
-        text = '',
-        texthl = 'DapBreakpoint',
-        linehl = 'DapBreakpoint',
-        numhl = 'DapBreakpoint',
-    },
-    rejected = {
-        text = "",
-        texthl = "DapBreakpint",
-        linehl = "DapBreakpoint",
-        numhl = "DapBreakpoint",
-    },
-    logpoint = {
-        text = '',
-        texthl = 'DapLogPoint',
-        linehl = 'DapLogPoint',
-        numhl = 'DapLogPoint',
-    },
-    stopped = {
-        text = '',
-        texthl = 'DapStopped',
-        linehl = 'DapStopped',
-        numhl = 'DapStopped',
-    },
+  error = {
+    text = "",
+    texthl = "DapBreakpoint_icon",
+    linehl = "DapBreakpoint",
+    numhl = "DapBreakpoint",
+  },
+  condition = {
+    text = '',
+    texthl = 'DapBreakpoint_icon',
+    linehl = 'DapBreakpoint',
+    numhl = 'DapBreakpoint',
+  },
+  rejected = {
+    text = "",
+    texthl = "DapBreakpint_icon",
+    linehl = "DapBreakpoint",
+    numhl = "DapBreakpoint",
+  },
+  logpoint = {
+    text = '',
+    texthl = 'DapLogPoint_icon',
+    linehl = 'DapLogPoint',
+    numhl = 'DapLogPoint',
+  },
+  stopped = {
+    text = '',
+    texthl = 'DapStopped_icon',
+    linehl = 'DapStopped',
+    numhl = 'DapStopped',
+  },
 }
 
 vim.fn.sign_define('DapBreakpoint', dap_breakpoint.error)
@@ -57,7 +73,55 @@ vim.fn.sign_define('DapLogPoint', dap_breakpoint.logpoint)
 vim.fn.sign_define('DapStopped', dap_breakpoint.stopped)
 
 local dapui = require("dapui")
-dapui.setup({})
+dapui.setup({
+  layouts = {
+    {
+      elements = {
+        {
+          id = "breakpoints",
+          size = 0.5
+        }, {
+          id = "stacks",
+          size = 0.5
+        } },
+      position = "left",
+      size = 35
+    },
+    {
+      elements = {
+        {
+          id = "scopes",
+          size = 0.70,
+        },
+        {
+          id = "watches",
+          size = 0.30
+        },
+      },
+      position = "left",
+      size = 60,
+    },
+    {
+      elements = {
+        {
+          id = "repl",
+          size = 0.5
+        }, {
+          id = "console",
+          size = 0.5
+        } },
+      position = "bottom",
+      size = 9
+    } },
+  mappings = {
+    edit = "e",
+    expand = { "<CR>", "<2-LeftMouse>" },
+    open = "o",
+    remove = "d",
+    repl = "r",
+    toggle = "b",
+  },
+})
 
 local dap = require("dap")
 dap.listeners.after.event_initialized["dapui_config"] = function()
