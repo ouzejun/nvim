daa7ec91fdbc12e3cd762e260aff658730be5fb8
yangtao <1157661964@qq.com>
2024-05-06 23:06:52 +0800

    gen add

--- lua/core/keymaps/copilotchat.lua
+++ lua/core/keymaps/copilotchat.lua
@@ -2,9 +2,9 @@ local keymap = vim.keymap
 
 -- normal
 keymap.set("n", "<leader>ic", "<cmd>CopilotChatToggle<CR>")
-keymap.set("n", "<leader>id", "<cmd>CopilotChatDocs<CR>")
-keymap.set("n", "<leader>io", "<cmd>CopilotChatOptimize<CR>")
-keymap.set("n", "<leader>if", "<cmd>CopilotChatFix<CR>")
+-- keymap.set("n", "<leader>id", "<cmd>CopilotChatDocs<CR>")
+-- keymap.set("n", "<leader>io", "<cmd>CopilotChatOptimize<CR>")
+-- keymap.set("n", "<leader>if", "<cmd>CopilotChatFix<CR>")
 keymap.set("n", "<leader>iq",
   function()
     local input = vim.fn.input("Quick Chat: ")
@@ -14,9 +14,9 @@ keymap.set("n", "<leader>iq",
   end
 )
 -- visual
-keymap.set("v", "<leader>id", "<cmd>CopilotChatDocs<CR>")
-keymap.set("v", "<leader>if", "<cmd>CopilotChatFix<CR>")
-keymap.set("v", "<leader>io", "<cmd>CopilotChatOptimize<CR>")
+-- keymap.set("v", "<leader>id", "<cmd>CopilotChatDocs<CR>")
+-- keymap.set("v", "<leader>if", "<cmd>CopilotChatFix<CR>")
+-- keymap.set("v", "<leader>io", "<cmd>CopilotChatOptimize<CR>")
 keymap.set("v", "<leader>iq",
   function()
     local input = vim.fn.input("Quick Chat: ")
---
+++ lua/core/keymaps/gen.lua
@@ -0,0 +1,1 @@
+vim.keymap.set({ 'n', 'v' }, '<leader>io', ':Gen<CR>')
