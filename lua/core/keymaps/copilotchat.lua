da931bbfe1de6f4e1682f8bdf2b4123e0a39e15e
yangtao <1157661964@qq.com>
2024-05-06 22:15:52 +0800

    ollama-temp

--- lua/core/keymaps/copilotchat.lua
+++ lua/core/keymaps/copilotchat.lua
@@ -3,7 +3,7 @@ local keymap = vim.keymap
 -- normal
 keymap.set("n", "<leader>ic", "<cmd>CopilotChatToggle<CR>")
 keymap.set("n", "<leader>id", "<cmd>CopilotChatDocs<CR>")
-keymap.set("n", "<leader>io", "<cmd>CopilotChatOptimize<CR>")
+-- keymap.set("n", "<leader>io", "<cmd>CopilotChatOptimize<CR>")
 keymap.set("n", "<leader>if", "<cmd>CopilotChatFix<CR>")
 keymap.set("n", "<leader>iq",
   function()
@@ -16,7 +16,7 @@ keymap.set("n", "<leader>iq",
 -- visual
 keymap.set("v", "<leader>id", "<cmd>CopilotChatDocs<CR>")
 keymap.set("v", "<leader>if", "<cmd>CopilotChatFix<CR>")
-keymap.set("v", "<leader>io", "<cmd>CopilotChatOptimize<CR>")
+-- keymap.set("v", "<leader>io", "<cmd>CopilotChatOptimize<CR>")
 keymap.set("v", "<leader>iq",
   function()
     local input = vim.fn.input("Quick Chat: ")
