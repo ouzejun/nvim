2dbcd13d5e361f907664c935c1e8251f229622ea
yangtao <1157661964@qq.com>
2024-08-13 23:20:32 +0800

    暂时禁用java lsp,出现了不明的问题

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -1,28 +1,29 @@
-return {
-  'nvim-java/nvim-java',
-  dependencies = {
-    'nvim-java/lua-async-await',
-    'nvim-java/nvim-java-core',
-    'nvim-java/nvim-java-test',
-    'nvim-java/nvim-java-dap',
-    'MunifTanjim/nui.nvim',
-    'neovim/nvim-lspconfig',
-    'mfussenegger/nvim-dap',
-    "nvim-java/nvim-java-refactor",
-    {
-      'williamboman/mason.nvim',
-      opts = {
-        registries = {
-          'github:nvim-java/mason-registry',
-          'github:mason-org/mason-registry',
-        },
-      },
-    }
-  },
-  require('lspconfig').jdtls.setup({
-    java_debug_adapter = {
-      enable = true,
-    },
-  })
-}
-
+return{}
+-- return {
+--   'nvim-java/nvim-java',
+--   dependencies = {
+--     'nvim-java/lua-async-await',
+--     'nvim-java/nvim-java-core',
+--     'nvim-java/nvim-java-test',
+--     'nvim-java/nvim-java-dap',
+--     'MunifTanjim/nui.nvim',
+--     'neovim/nvim-lspconfig',
+--     'mfussenegger/nvim-dap',
+--     "nvim-java/nvim-java-refactor",
+--     {
+--       'williamboman/mason.nvim',
+--       opts = {
+--         registries = {
+--           'github:nvim-java/mason-registry',
+--           'github:mason-org/mason-registry',
+--         },
+--       },
+--     }
+--   },
+--   require('lspconfig').jdtls.setup({
+--     java_debug_adapter = {
+--       enable = true,
+--     },
+--   })
+-- }
+--
